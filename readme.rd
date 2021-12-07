54735507b47463cbd8de141a0c35dc887a7e86e2
Dimitri Rybak <dima_rybak@yahoo.de>
2021-12-07 18:01:09 +0100

    working prod service

--- readme.rd
+++ readme.rd
@@ -1,5 +1,8 @@
-#start service
-python remove_background.py
+# run service on windows
+waitress-serve --listen=*:8080 aiservice:app
+
+aiservice - name of python file with flask app
+app - variable of flask service: "app = Flask(__name__)"
 
 #test service
 python test.py
--- test.py
+++ test.py
@@ -3,14 +3,17 @@ from PIL import Image
 import io
 import os
 import json
+import time
+
+start_time = time.time()
 
 #resp = requests.post("http://localhost:5000", files={'file': open('ServicePic/NarutoHokage.png', 'rb')})
 ref_size = 500
 
-url = "http://localhost:5000"
+url = "http://dimi-nas.synology.me:8080/"
 data = {'ref-size': ref_size,
         'greyscale': 'Ye' } # noly "Yes" will give the geyscale picture
-filename = 'ServicePic/Naruto.png'
+filename = 'Naruto.png'
 file_output_name = "Naruto{0}.png".format(ref_size)
 
 files = [
@@ -23,4 +26,6 @@ print(resp.status_code)
 #print(resp.content)
 #print(resp.json())
 
-Image.open(io.BytesIO(resp.content)).save(os.path.join('ServicePicOutput', file_output_name))
+Image.open(io.BytesIO(resp.content)).save(os.path.join('', file_output_name))
+
+print( time.time() - start_time )
